cff5c6ef36b2b1254630173a4407cc4cc0d3980b
mizumoto <tmizumot04@gmail.com>
2017-02-28 19:50:10 +0900

    sample

---
+++ utaehon/js/swipe.js
@@ -0,0 +1,13 @@
+Flipsnap('.flipsnap', {
+    distance: 325
+});
+
+// $(function(){
+//     SampleClass.init();
+// });
+//
+// var SampleClass = {
+//     init : function () {
+//         Flipsnap("#sampleFlipsnap", {distance:60, maxPoint:3});
+//     },
+// };
