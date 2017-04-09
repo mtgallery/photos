62c04d953b306ba2d278645bfb2588c5c16170a8
mizumoto <tmizumot04@gmail.com>
2017-04-09 19:37:15 +0900

    a

--- PortfolioSite/js/scroll-header.js
+++ PortfolioSite/js/scroll-header.js
@@ -1,14 +1,14 @@
 function init() {
 // スクロールして何ピクセルでアニメーションさせるか
-var px_change   = 1000;
+var px_change   = 510;
 // スクロールのイベントハンドラを登録
 window.addEventListener('scroll', function(evt){
 // 変化するポイントまでスクロールしたらクラスを追加
 if ( $(window).scrollTop() > px_change ) {
-$("header").addClass("changeColor-gray");
+$("header").addClass("bgColor");
 // 変化するポイント以前であればクラスを削除
-} else if ( $("header").hasClass("changeColor-white") ) {
-$("header").removeClass("changeColor-gray");
+} else if ( $("header").hasClass("bgColor") ) {
+$("header").removeClass("bgColor");
 }
 });
 }
