56d96ce37e87406e995512bdf798540cec347c75
mizumoto <tmizumot04@gmail.com>
2017-04-12 19:30:14 +0900

    a

--- PortfolioSite/js/loadarea.js
+++ PortfolioSite/js/loadarea.js
@@ -10,19 +10,19 @@ $(function(){
 
         if(loopCont < maxNum){
             $.each(new Array(loadNum),function(i){
-                $setArea.append('<article class="list"' + '><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/works/img' + ((childLengh)+((i)+1)) + '.png" alt="" /></a>');
+                $setArea.append('<article class="list list-mv07 mv07"><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/works/img' + ((childLengh)+((i)+1)) + '.png)" /></a>');
             });
         } else if(loopCont >= maxNum){
             var overCont = (loopCont)-(maxNum);
             var adjustCont = (loadNum)-(overCont);
 
             $.each(new Array(adjustCont),function(i){
-                $setArea.append('<article class="list"' + '><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/works/img' + ((childLengh)+((i)+1)) + '.png" alt="" /></a>');
+                $setArea.append('<article class="list list-mv07 mv07"><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/works/img' + ((childLengh)+((i)+1)) + '.png)" /></a>');
                 $setMore.remove();
             });
         }
     });
-    $(window).load(function(){
-        $setMore.click();
-    });
+    // $(window).load(function(){
+    //     $setMore.click();
+    // });
 });
