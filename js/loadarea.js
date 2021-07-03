6c0c82f4b9fffbc899aecc21e796fcde4edeb4e0
mizumoto <tmizumot04@gmail.com>
2021-07-03 18:16:58 +0900

    a
    a

--- js/loadarea.js
+++ js/loadarea.js
@@ -2,7 +2,7 @@ $(function(){
     var $setArea = $('#loadarea'),
     $setMore = $('#loadmore'),
     loadNum = 3,
-    maxNum = 11;
+    maxNum = 31;
 
     $setMore.click(function(){
         var childLengh = $setArea.children('div').length;
@@ -10,14 +10,14 @@ $(function(){
 
         if(loopCont < maxNum){
             $.each(new Array(loadNum),function(i){
-                $setArea.append('<div class="list list-item list-mv07 mv07"><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/gallery/img' + ((childLengh)+((i)+1)) + '.png)" /></a>');
+                $setArea.append('<div class="list list-item list-mv07 mv07"><a href=""' + ' target="_blank"><div class="photo" style="background-image:url(images/gallery/img' + ((childLengh)+((i)+1)) + '.jpg)" /></a>');
             });
         } else if(loopCont >= maxNum){
             var overCont = (loopCont)-(maxNum);
             var adjustCont = (loadNum)-(overCont);
 
             $.each(new Array(adjustCont),function(i){
-                $setArea.append('<div class="list list-item list-mv07 mv07"><a href="https://www.behance.net/tmizumot0430"' + ' target="_blank"><div class="photo" style="background-image:url(images/gallery/img' + ((childLengh)+((i)+1)) + '.png)" /></a>');
+                $setArea.append('<div class="list list-item list-mv07 mv07"><a href=""' + ' target="_blank"><div class="photo" style="background-image:url(images/gallery/img' + ((childLengh)+((i)+1)) + '.jpg)" /></a>');
                 $setMore.remove();
             });
         }
