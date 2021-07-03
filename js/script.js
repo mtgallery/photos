6e1d58183338e4946e842499f94e2ad6cec55d2a
mizumoto <tmizumot04@gmail.com>
2021-07-04 00:33:25 +0900

    a
    a

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 //DOMを読み込んだら
 $(function(){
 
+
     // .pagetopのaをクリックしたら
     $('.pagetop a').on('click', function(evt){
         evt.preventDefault();//イベントの停止
-        console.log(evt.target);
 
         //ページ全体を一番上にスクロールされる
         $('html, body').animate({scrollTop:0},1000,function(){
-            console.log('ページトップのアニメーションが終わったよ');
         });
     });
 
@@ -19,14 +18,20 @@ $(function(){
 
         //1 リンク先の変数を付ける
         var targetName = $(this).attr('href');
+        console.log(targetName);
 
         //2 targetNameの座標を保存する変数を作る
         var targetPos = $(targetName).offset().top; //offset 2つの値が仕様 object {top left}
+        console.log(targetPos);
 
         // $('html, body').animate({scrollTop:targetPos - 70},1000);
         $('html, body').animate({scrollTop:targetPos - 70},{duration:1000, easing:'easeOutQuad', complete:callBack});
 
     });
+
+    function callBack(){
+        console.log('アニメーション終わったよ');
+    }
 
 
 
