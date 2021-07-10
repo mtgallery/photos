b5fe0d9178f63eac7a4b41bebf7e2e2cedd7b9b9
mizumoto <tmizumot04@gmail.com>
2021-07-10 15:38:18 +0900

    a
    a

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 //DOMを読み込んだら
 $(function(){
 
+
+    // .pagetopのaをクリックしたら
+    $('.pagetop a').on('click', function(evt){
+        evt.preventDefault();//イベントの停止
+
+        //ページ全体を一番上にスクロールされる
+        $('html, body').animate({scrollTop:0},1000,function(){
+        });
+    });
+
     // header ul li a をクリックしたら
     $('.contents-bar a, .full-nav a').on('click', function(evt){
         evt.preventDefault();
