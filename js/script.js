c608a82b161d0811e8167ddb6b1368f0dc5837a0
mizumoto <tmizumot04@gmail.com>
2021-07-10 16:18:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,22 +11,24 @@ $(function(){
         });
     });
 
-
-    var headerHeight = $('header .contents_wrap').outerHeight();
-    var urlHash = location.hash;
-    if(urlHash) {
+    $(function(){
+      var headerHeight = $('header .contents_wrap').outerHeight();
+      var urlHash = location.hash;
+      if(urlHash) {
         $('body,html').stop().scrollTop(0);
         setTimeout(function(){
-            var target = $(urlHash);
-            var position = target.offset().top - headerHeight;
-            $('body,html').stop().animate({scrollTop:position}, 1000);
+          var target = $(urlHash);
+          var position = target.offset().top - headerHeight;
+          $('body,html').stop().animate({scrollTop:position}, 500);
         }, 100);
-    }
-    $('.contents-bar a[href^="#"], .full-nav a[href^="#"]').click(function() {
+      }
+      $('a[href^=#]').click(function(){
         var href= $(this).attr("href");
         var target = $(href);
         var position = target.offset().top - headerHeight;
-        $('body,html').stop().animate({scrollTop:position}, 1000);
+        $('body,html').stop().animate({scrollTop:position}, 500);
+        return false;
+      });
     });
 
 
