fd4a8477526ae688438f123650170940c2769b0d
mizumoto <tmizumot04@gmail.com>
2021-07-10 17:01:43 +0900

    a

--- design/js/loadarea.js
+++ design/js/loadarea.js
@@ -1,5 +1,5 @@
 $(function(){
-    var $setArea = $('#loadarea'),
+    var $setArea = $('.load_area'),
     $setMore = $('#loadmore01'),
     loadNum = 5,
     maxNum = 9;
@@ -22,7 +22,30 @@ $(function(){
             });
         }
     });
-    // $(window).load(function(){
-    //     $setMore.click();
-    // });
+});
+
+$(function(){
+    var $setArea = $('.load_area'),
+    $setMore = $('#loadmore02'),
+    loadNum = 5,
+    maxNum = 9;
+
+    $setMore.click(function(){
+        var childLengh = $setArea.children('div').length;
+        var loopCont = (childLengh)+(loadNum);
+
+        if(loopCont < maxNum){
+            $.each(new Array(loadNum),function(i){
+                $setArea.append('<div class="list list-item list-mv07 mv07"><div class="photo bl_galleryImgBlock_item js_modalButton" style="background-image:url(https://mtgallery.github.io/portfolio/design/images/link_img' + ((childLengh)+((i)+1)) + '.jpg);" data-src="https://mtgallery.github.io/portfolio/design/images/link_img' + ((childLengh)+((i)+1)) + '.jpg" /></div>');
+            });
+        } else if(loopCont >= maxNum){
+            var overCont = (loopCont)-(maxNum);
+            var adjustCont = (loadNum)-(overCont);
+
+            $.each(new Array(adjustCont),function(i){
+                $setArea.append('<div class="list list-item list-mv07 mv07"><div class="photo bl_galleryImgBlock_item js_modalButton" style="background-image:url(https://mtgallery.github.io/portfolio/design/images/link_img' + ((childLengh)+((i)+1)) + '.jpg);" data-src="https://mtgallery.github.io/portfolio/design/images/link_img' + ((childLengh)+((i)+1)) + '.jpg" /></div>');
+                $setMore.remove();
+            });
+        }
+    });
 });
